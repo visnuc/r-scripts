af042cfcaa6bbdae25d941a96a3c2952a0ab8840
pritom-vc <pr3370m@gmail.com>
2021-03-04 08:44:54 +0600

    working on data manipulation

--- r-script-merge.R
+++ r-script-merge.R
@@ -1,6 +1,6 @@
-# ============================
-# MERGING AND CLEANING DATA 
-# ============================
+# ========================
+#   DATA MANIPULATION      
+# ========================
 
 # importing data 
 dataRaw <-read.csv(file.choose(), header=T) 
@@ -14,7 +14,6 @@ names(washMain)
 View(vaxTiter)
 names(vaxTiter)
 
-install.packages("tidyverse")
 library(tidyverse)
 # vtAll <- vaxTiter %>%
 #   select(Pid, agedays, everything())
@@ -37,49 +36,15 @@ vt <- vaxTiter %>%
 
 View(vt)
 
-# nrow(vt)
-# nrow(washMain)
-# nrow(washMain2)
-# 
-# ncol(vt)
-# ncol(vaxTiter)
-# ncol(washMain)
+nrow(vt)
+nrow(washMain)
+nrow(washMain2)
+
+ncol(vt)
+ncol(vaxTiter)
+ncol(washMain)
 
 washMain2 <- merge(washMain, vt, by = "pid", all.x = T)
 write.table(washMain2, file = "data_wash_2_v20210302.csv", sep = ",", row.names = F)
 
 
-# testing codes 
-data()
-View(starwars)
-sw <- starwars %>%
-  select(gender, mass, height, species) %>%
-  na.omit() %>%
-  filter(species == "Human") %>%
-  mutate(height = height /100) %>%
-  mutate(BMI = mass/height^2) %>%
-  group_by(gender) %>%
-  summarise(Average_BMI = mean(BMI))
-
-view(sw)
-
-vt <- vaxTiter %>%
-  select(Pid, agedays, target_month, 
-         log2mea, log2tet, log2per, 
-         log2rotaa, log2rotag,
-         log2poligg, log2pol1n, log2pol2n, log2pol3n) %>%
-  rename(pid = Pid, 
-         log2meas = log2mea, 
-         log2teta = log2tet, 
-         log2pertu = log2per, 
-         log2rotaA = log2rotaa, 
-         log2rotaG = log2rotag, 
-         log2polG = log2poligg, 
-         log2pol1 = log2pol1n, 
-         log2pol2 = log2pol2n, 
-         log2pol3 = log2pol3n) %>%
-  na.omit() %>%
-  group_by(target_month) %>%
-  summarise(Average_measles_titer = mean(log2meas))
-
-vt
