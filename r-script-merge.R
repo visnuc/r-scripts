d2b62efd6998c5b4c6bb41d97d675cedce488801
pritom-vc <pr3370m@gmail.com>
2021-03-09 12:33:34 +0600

    more commits

--- r-script-merge.R
+++ r-script-merge.R
@@ -7,29 +7,39 @@ setwd("C:/Users/visnu.pritom/Dropbox/Projects_DSDC/study_stunting_vs_vaccination
 getwd()
 
 # importing data 
-dataRaw <-read.csv(file.choose(), header=T) 
+dataRaw <- read.csv(file.choose(), header=T)
 
-washMain <- dataRaw
-vaxTiter <- dataRaw
+washMain <- read.csv("C:/Users/visnu.pritom/Dropbox/Projects_DSDC/study_stunting_vs_vaccination_dsdc/data/data_wash_rev_final_v20210228.csv", 
+                     header=T) 
+vtMain <- read.csv("C:/Users/visnu.pritom/Dropbox/Projects_DSDC/study_stunting_vs_vaccination_dsdc/data/data_vax_titer_wdates_v20210228.csv", 
+                     header=T) 
 
-# # gets number of rows 
+attach(c(washMain, vtMain))
+View(washMain)
+View(vtMain)
+nrow(washMain)
+nrow(vtMain)
+
+# # number of rows and columns
 # nrow(vt)
-# # gets number of columns 
 # ncol(vt)
 # # gets names of variables
 # names(vt)
-# # view data set, from tidyverse 
+# # view data set, from tidyverse
 # view(vt)
-# view data set, default 
+# # view data set, default
 # View(vt)
+
+names(washMain)
+names(vtMain)
 
 # install.packages("tidyverse")
 library(tidyverse)
 # vtAll <- vaxTiter %>%
 #   select(Pid, agedays, everything())
 
-vt <- vaxTiter %>%
-  select(Pid, agedays, 
+vt2 <- vtMain %>%
+  select(Pid, agedays, target_month, 
          log2mea, log2tet, log2per, 
          log2rotaa, log2rotag,
          log2poligg, log2pol1n, log2pol2n, log2pol3n) %>%
@@ -44,52 +54,83 @@ vt <- vaxTiter %>%
          log2pol2 = log2pol2n, 
          log2pol3 = log2pol3n)
 
+View(vt2)
+nrow(vt2)
+write.table(vt2, 
+            file = "vt2_v20210309.csv", 
+            sep = ",", 
+            row.names = F)
 
-washMain2 <- merge(washMain, vt, by = "pid", all.x = T)
-write.table(washMain2, file = "data_wash_2_v20210302.csv", sep = "," 
-            , row.names = F)
+table(target_month)
+str(vt2)
 
-# ---------------------
-#   testing codes 
-# ---------------------
-# # lists preloaded data 
-# data()
+wm2 <- merge(washMain, vt2, 
+             by = "pid", 
+             all.x = T) %>%
+  na.omit()
 
-vtRaw <- vaxTiter %>%
-  select(Pid, agedays, target_month, 
-         log2mea, log2tet, log2per, 
-         log2rotaa, log2rotag,
-         log2poligg, log2pol1n, log2pol2n, log2pol3n) %>%
-  rename(pid = Pid, 
-         log2meas = log2mea, 
-         log2teta = log2tet, 
-         log2pertu = log2per, 
-         log2rotaA = log2rotaa, 
-         log2rotaG = log2rotag, 
-         log2polG = log2poligg, 
-         log2pol1 = log2pol1n, 
-         log2pol2 = log2pol2n, 
-         log2pol3 = log2pol3n) %>%
-  na.omit() %>%
-  group_by(target_month) %>%
-  summarise(avg_titer_measles = mean(log2meas), 
-            avg_titer_tetanus = mean(log2teta), 
-            avg_titer_pertussis = mean(log2pertu), 
-            avg_titer_rotavirusIgA = mean(log2rotaA), 
-            avg_titer_rotavirusIgG = mean(log2rotaG), 
-            avg_titer_polioIgG = mean(log2polG), 
-            avg_titer_polioS1 = mean(log2pol1), 
-            avg_titer_polioS2 = mean(log2pol2), 
-            avg_titer_polioS3 = mean(log2pol3)
-            ) 
+nrow(wm2)
+View(wm2)
+table(wm2$target_month)
 
-View(vtRaw)
-write.table(vtRaw, file = "tmp_titer_status.csv", sep = "," 
-            , row.names = F)
+write.table(wm2,
+            file = "wm2_v20210309.csv",
+            sep = ",",
+            row.names = F)
 
-vtByMonth <- vaxTiter %>%
-  count(target_month)
+class(wm2$target_month)
+class(wm2$log2meas)
+wm2$target_month <- as.factor(wm2$target_month)
+str(wm2)
+summary(wm2)
 
-View(vtByMonth)
-write.table(vtByMonth, file = "tmp_month_counts.csv", sep = "," 
-            , row.names = F)
+# # -----------------------
+# #   trials and errors
+# # -----------------------
+# # # lists preloaded data 
+# # data()
+# 
+# vt2 <- vtMain %>%
+#   select(Pid, agedays, target_month, 
+#          log2mea, log2tet, log2per, 
+#          log2rotaa, log2rotag,
+#          log2poligg, log2pol1n, log2pol2n, log2pol3n) %>%
+#   rename(pid = Pid, 
+#          log2meas = log2mea, 
+#          log2teta = log2tet, 
+#          log2pertu = log2per, 
+#          log2rotaA = log2rotaa, 
+#          log2rotaG = log2rotag, 
+#          log2polG = log2poligg, 
+#          log2pol1 = log2pol1n, 
+#          log2pol2 = log2pol2n, 
+#          log2pol3 = log2pol3n) %>%
+#   na.omit() %>%
+#   group_by(target_month) %>%
+#   summarise(avg_titer_measles = mean(log2meas), 
+#             avg_titer_tetanus = mean(log2teta), 
+#             avg_titer_pertussis = mean(log2pertu), 
+#             avg_titer_rotavirusIgA = mean(log2rotaA), 
+#             avg_titer_rotavirusIgG = mean(log2rotaG), 
+#             avg_titer_polioIgG = mean(log2polG), 
+#             avg_titer_polioS1 = mean(log2pol1), 
+#             avg_titer_polioS2 = mean(log2pol2), 
+#             avg_titer_polioS3 = mean(log2pol3)
+#             ) 
+# 
+# View(vtRaw)
+# write.table(vtRaw, 
+#             file = "tmp_titer_status.csv", 
+#             sep = ",", 
+#             row.names = F)
+# 
+# vtByMonth <- vaxTiter %>%
+#   count(target_month)
+# 
+# View(vtByMonth)
+# write.table(vtByMonth, 
+#             file = "tmp_month_counts.csv", 
+#             sep = ",", 
+#             row.names = F)
+
+detach(c(washMain, vtMain))
