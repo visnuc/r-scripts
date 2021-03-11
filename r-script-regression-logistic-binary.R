525bd0892bdee43cb40ed6e9aac2a782751796bf
pritom-vc <pr3370m@gmail.com>
2021-03-11 16:46:54 +0600

    more r codes

---
+++ r-script-regression-logistic-binary.R
@@ -0,0 +1,57 @@
+# =================================
+#   Binary logistic regression 
+# =================================
+
+# ---------------------------------
+library(foreign)
+library(tidyverse)
+
+# ---------------------------------
+# data import export 
+# t3ss <- read.spss(file.choose(), header=T)
+t3ss <- read.csv(file.choose(), header=T)
+
+attach(t3ss)
+names(t3ss)
+View(t3ss2)
+dim(t3ss)
+str(t3ss2)
+
+write.table(t3ssReg, 
+            file = "t3ss_small_20210311_coded.csv", 
+            sep = ",", 
+            row.names = F)
+
+# ---------------------------------
+# recoding
+t3ssReg <- t3ss %>%
+  select(lab_id, 
+         cf_temp_fever_01, ho_bloody_01, ho_cough_01, 
+         ho_mucoid_01, ho_re_str_01, 
+         lab_ial_01, lab_set_01, lab_virB_01, lab_ipaBCD_01, 
+         lab_ipgB1_01, lab_icsB_01, lab_ipgD_01, lab_ipgF_01, 
+         lab_mxiH_01, lab_mxiK_01, lab_mxiE_01, lab_mxiC_01, 
+         lab_spa47_01, lab_spa32_01) %>%
+  mutate(cf_temp_fever_01 = recode(cf_temp_fever_01, Yes = 1, No = 0), 
+         ho_bloody_01 = recode(ho_bloody_01, Yes = 1, No = 0),
+         ho_cough_01 = recode(ho_cough_01, Yes = 1, No = 0),
+         ho_mucoid_01 = recode(ho_mucoid_01, Yes = 1, No = 0),
+         ho_re_str_01 = recode(ho_re_str_01, Yes = 1, No = 0),
+         lab_ial_01 = recode(lab_ial_01, Positive = 1, Negative = 0),
+         lab_set_01 = recode(lab_set_01, Positive = 1, Negative = 0),
+         lab_virB_01 = recode(lab_virB_01, Positive = 1, Negative = 0),
+         lab_ipaBCD_01 = recode(lab_ipaBCD_01, Positive = 1, Negative = 0),
+         lab_ipgB1_01 = recode(lab_ipgB1_01, Positive = 1, Negative = 0),
+         lab_icsB_01 = recode(lab_icsB_01, Positive = 1, Negative = 0),
+         lab_ipgD_01 = recode(lab_ipgD_01, Positive = 1, Negative = 0),
+         lab_ipgF_01 = recode(lab_ipgF_01, Positive = 1, Negative = 0),
+         lab_mxiH_01 = recode(lab_mxiH_01, Positive = 1, Negative = 0),
+         lab_mxiK_01 = recode(lab_mxiK_01, Positive = 1, Negative = 0),
+         lab_mxiE_01 = recode(lab_mxiE_01, Positive = 1, Negative = 0),
+         lab_mxiC_01 = recode(lab_mxiC_01, Positive = 1, Negative = 0),
+         lab_spa47_01 = recode(lab_spa47_01, Positive = 1, Negative = 0),
+         lab_spa32_01 = recode(lab_spa32_01, Positive = 1, Negative = 0))
+
+# View(t3ssReg)
+# str(t3ssReg)
+# names(t3ssReg)
