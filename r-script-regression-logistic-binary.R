8abdb02d0d1aed19679dd6365e9da219d6224b56
pritom-vc <pr3370m@gmail.com>
2021-03-11 22:10:05 +0600

    more R codes

--- r-script-regression-logistic-binary.R
+++ r-script-regression-logistic-binary.R
@@ -5,6 +5,7 @@
 # ---------------------------------
 library(foreign)
 library(tidyverse)
+library(MASS)
 
 # ---------------------------------
 # data import export 
@@ -13,9 +14,9 @@ t3ss <- read.csv(file.choose(), header=T)
 
 attach(t3ss)
 names(t3ss)
-View(t3ss2)
+View(t3ss)
 dim(t3ss)
-str(t3ss2)
+str(t3ss)
 
 write.table(t3ssReg, 
             file = "t3ss_small_20210311_coded.csv", 
@@ -55,3 +56,49 @@ t3ssReg <- t3ss %>%
 # View(t3ssReg)
 # str(t3ssReg)
 # names(t3ssReg)
+
+# ---------------------------------
+# Binary logistic regression - unadjusted
+
+# t3ssRegUnadj <- glm(cf_temp_fever_01 ~ lab_set_01, 
+#                       family = binomial(link = "logit"), 
+#                       data = t3ss)
+
+# t3ssRegUnadj <- glm(ho_bloody_01 ~ lab_set_01 + lab_icsB_01, 
+#                     family = binomial(link = "logit"), 
+#                     data = t3ss)
+
+# t3ssRegUnadj <- glm(ho_cough_01 ~ lab_set_01 + lab_mxiE_01, 
+#                     family = binomial(link = "logit"), 
+#                     data = t3ss)
+
+# t3ssRegUnadj <- glm(ho_mucoid_01 ~ lab_set_01 + lab_icsB_01, 
+#                     family = binomial(link = "logit"), 
+#                     data = t3ss)
+
+t3ssRegUnadj <- glm(ho_re_str_01 ~ lab_set_01 + lab_icsB_01 + lab_mxiC_01, 
+                    family = binomial(link = "logit"), 
+                    data = t3ss)
+
+summary(t3ssRegUnadj)
+
+# OR & 95% CI
+round(exp(cbind(coef(t3ssRegUnadj), confint(t3ssRegUnadj))), 3)
+
+
+# Binary logistic regression - adjusted
+
+t3ssRegAdj <- glm(ho_mucoid_01 ~ lab_ial_01 + lab_set_01 + 
+                    lab_virB_01 + lab_ipaBCD_01 + 
+                    lab_ipgB1_01 + lab_icsB_01 + 
+                    lab_ipgD_01+ lab_ipgF_01 + 
+                    lab_mxiH_01 + lab_mxiK_01 + 
+                    lab_mxiE_01 + lab_mxiC_01 + 
+                    lab_spa47_01 + lab_spa32_01, 
+                  family = binomial(link = "logit"), 
+                  data = t3ss)
+
+summary(t3ssRegAdj)
+
+# OR & 95% CI
+round(exp(cbind(coef(t3ssRegAdj), confint(t3ssRegAdj))), 3)
