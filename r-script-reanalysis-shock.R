49adf49a7143671dbbca19e41e4a0bd7702726b7
pritom-vc <pr3370m@gmail.com>
2021-04-16 03:00:15 +0600

    more R codes

--- r-script-reanalysis-shock.R
+++ r-script-reanalysis-shock.R
@@ -6,11 +6,11 @@ library(car); library(codebook); library(coefplot)
 library(datasets); library(devtools); library(dplyr)
 library(e1071)
 library(foreign)
-library(ggplot2); library(ggpubr); library(ggthemes); library(grid); library(gridExtra)
+library(ggplot2); library(ggpubr); library(ggthemes)
 library(haven)
 library(MASS); library(moments)
-library(plotrix); library(plyr)
-library(RColorBrewer); library(reshape); library(rstatix)
+library(pastecs); library(plotrix); library(plyr); library(psych)
+library(Rcmdr); library(RColorBrewer); library(reshape); library(rstatix)
 library(tidyverse)
 library(utils)
 
@@ -31,13 +31,15 @@ dir()
 # ---------------------------------
 #     data import 
 # ---------------------------------
-shock <- read.csv(file.choose(), header=T)
-
+shock <- read.csv(file.choose(), header = T)
+shock <- read.delim(file.choose(), header = T)
+shock <- read.dta(file.choose())
+shock <- read.spss(file.choose(), header = T)
 
 # ---------------------------------
 #     others 
 # ---------------------------------
-attach(shock); names(shock)
+attach(shock); names(shock); View(shock)
 View(shock)
 nrow(shock)
 dim(shock)
@@ -51,55 +53,14 @@ s12 <- merge(shock, shockTmp, by = "pp_pid", all.x = T)
 # ---------------------------------
 #     export data 
 # ---------------------------------
-write.table(shock, 
-            file = "tmp.csv", 
-            sep = ",", 
-            row.names = F)
+write.table(shock, file = "new.csv", sep = ",", row.names = F)
 
 
 # ---------------------------------
 #     subsetting 
 # ---------------------------------
-shock2 <- shock %>%
-  select(pp_pid, pp_age_est_yr, pp_sex_bivar, 
-         socio_restype_notbuilding_bivar, socio_water_bivar, 
-         pedi_imm_bivar, pedi_dev_bivar, pedi_birth_bivar, 
-         pedi_feed_bivar, anthro_wt, anthro_ht, 
-         hosp_hosp_stay, 
-         cc_diarr_bivar, cc_diarr_d, cc_st_cons_bivar, cc_vis_bld_bivar, 
-         cc_cough_bivar, cc_cough_d, cc_resp_dist_bivar, cc_resp_dist_d, 
-         cc_fever_bivar, cc_fever_d, 
-         prevrx_currdx_abx_bivar, past_chd_bivar, past_tri21_bivar, 
-         cf_rr, cf_rr_fast_br_bivar, cf_lung_crepts_bivar, 
-         cf_lcwi_bivar, cf_spo2, cf_hypoxia_bivar, cf_p_rate, 
-         cf_p_char_bivar, cf_map_bivar, cf_crt_bivar, 
-         cf_peri_bivar, cf_uo_bivar, cf_dh_bivar, 
-         cf_ede_bivar, cf_hightemp_bivar, cf_abd_dist_bivar, 
-         cf_bs_slug_bivar, cf_rbs_bivar, 
-         mx_line_1_bivar, mx_line_2_bivar, mx_line_3_bivar, 
-         mx_line_4_bivar, mx_steroid_bivar, 
-         dx_sclere_bivar, dx_s_pneu_bivar, dx_haip_bivar, 
-         dx_conv_bivar, dx_hypogly_bivar, dx_hyper_na_bivar, 
-         dx_ei_oth_bivar, dx_ileus_bivar, dx_samuw_bivar, 
-         dx_awd_bivar, dx_id_bivar, dx_pd_bivar, dx_aki_bivar,
-         inv_bl_cs_bivar, inv_rs_cs_bivar, inv_st_cs_bivar,
-         inv_st_rs_cs_bivar, inv_u_cs_bivar, inv_csf_cs_bivar,
-         inv_trac_cs_bivar, inv_trac_cs_multi_org_bivar,
-         inv_biochem_hyper_na_bivar, inv_biochem_hypo_na_bivar,
-         inv_biochem_hyper_k_bivar, inv_biochem_hypo_k_bivar,
-         inv_biochem_met_acido_bivar, inv_biochem_ag_high_bivar,
-         inv_biochem_cr_hi_bivar, inv_biochem_cr_aki_bivar,
-         inv_biochem_ca_low_bivar,
-         inv_biochem_hi_mg_bivar, inv_biochem_low_mg_bivar,
-         inv_hem_mod_anemia_9.3, inv_hem_leukocytosis_bivar,
-         inv_hem_neutrophilia_bivar, inv_hem_neutropenia_bivar,
-         inv_hem_lymphocytosis_bivar, inv_hem_lympho_penia_bivar,
-         inv_hem_thombocytopenia_bivar,
-         gap_shk_bt, gap_shk_bt_2h, gap_shk_bt_3h,
-         gap_shk_bt_4h, gap_shk_bt_6h,
-         outcome_bivar, bt_cause_bivar,
-         reg_mod_anemia, reg_meropenem, reg_steroids, reg_outcome,
-         reg_sclerema, reg_sev_pneumonia, reg_hai_hap) 
+shock2 <- shockTmp %>%
+  select(pp_pid, mx_ino) 
 
 
 # ---------------------------------
@@ -232,6 +193,10 @@ shock$mx_line_2_bivar <- recode(shock$mx_line_2_bivar, "yes" = 1, "no" = 0)
 shock$mx_line_3_bivar <- recode(shock$mx_line_3_bivar, "yes" = 1, "no" = 0)
 shock$mx_line_4_bivar <- recode(shock$mx_line_4_bivar, "yes" = 1, "no" = 0)
 shock$mx_ino <- recode(shock$mx_ino, "0" = 0, "one" = 1, "two" = 2, "all three" = 3)
+shock$mx_vasopressor <- recode(shock$mx_ino, "0" = 0, "1" = 0, "2" = 1, "3" = 1)
+shock$mx_dopamine <- recode(shock$mx_ino, "0" = 0, "1" = 1, "2" = 0, "3" = 0)
+shock$mx_adrenaline <- recode(shock$mx_ino, "0" = 0, "1" = 0, "2" = 1, "3" = 0)
+shock$mx_noradrenaline <- recode(shock$mx_ino, "0" = 0, "1" = 0, "2" = 0, "3" = 1)
 shock$mx_steroid_bivar <- recode(shock$mx_steroid_bivar, "yes" = 1, "no" = 0)
 
 shock$dx_sclere_bivar <- recode(shock$dx_sclere_bivar, "yes" = 1, "no" = 0)
@@ -331,6 +296,10 @@ continTable <- table(shock$reg_outcome, shock$dx_id_bivar)
 continTable <- table(shock$reg_outcome, shock$mx_line_1_bivar)
 continTable <- table(shock$reg_outcome, shock$mx_line_2_bivar)
 continTable <- table(shock$reg_outcome, shock$mx_line_3_bivar)
+continTable <- table(shock$reg_outcome, shock$mx_vasopressor)
+continTable <- table(shock$reg_outcome, shock$mx_dopamine); continTable
+continTable <- table(shock$reg_outcome, shock$mx_adrenaline); continTable
+continTable <- table(shock$reg_outcome, shock$mx_noradrenaline); continTable
 continTable <- table(shock$reg_outcome, shock$reg_meropenem)
 continTable <- table(shock$reg_outcome, shock$reg_steroids)
 continTable <- table(shock$reg_outcome, shock$gap_shk_bt_3h)
@@ -339,13 +308,11 @@ continTable <- table(shock$reg_outcome, shock$gap_shk_bt_6h)
 continTable <- table(shock$reg_outcome, shock$gap_shk_bt_12h)
 continTable <- table(shock$reg_outcome, shock$gapShkBt12h)
 
-rownames(continTable) <- c("Survival", "Death")
+rownames(continTable) <- c("Survival", "Death"); continTable
 colnames(continTable) <- c("Female", "Male")
 colnames(continTable) <- c("No_org", "Growth")
 colnames(continTable) <- c("LUCS", "NVD")
-colnames(continTable) <- c("No", "Yes")
-
-continTable
+colnames(continTable) <- c("No", "Yes"); continTable
 
 # relative frequencies percentage 
 prop.table(continTable)*100
@@ -447,42 +414,17 @@ shock %>% group_by(reg_outcome) %>% get_summary_stats(gap_shk_bt, type = "median
 
 
 # ---------------------------------
-#   Binary logistic regression - unadjusted
+#   binary logistic regression - unadjusted
 # ---------------------------------
-shbtRegUnadj <- glm(reg_outcome ~ reg_meropenem, # Pr 0.0287
-                  family = binomial(link = "logit"),
-                  data = shbtSmall2)
-
-shbtRegUnadj <- glm(reg_outcome ~ reg_steroids, # Pr 0.00106
-                  family = binomial(link = "logit"),
-                  data = shbtSmall2)
-# 
-# shbtRegUnadj <- glm(reg_outcome ~ reg_mod_anemia, # Pr 0.251
-#                     family = binomial(link = "logit"), 
-#                     data = shbtSmall2)
-
-# shbtRegUnadj <- glm(reg_outcome ~ reg_sev_pneumonia, # Pr 0.2549
-#                     family = binomial(link = "logit"), 
-#                     data = shbtSmall2)
-# 
-# shbtRegUnadj <- glm(reg_outcome ~ reg_sclerema, # Pr 0.178
-#                     family = binomial(link = "logit"), 
-#                     data = shbtSmall2)
-
-summary(shbtRegUnadj)
-
-# OR & 95% CI
-round(exp(cbind(coef(shbtRegUnadj), confint(shbtRegUnadj))), 3)
-
 shkRegUnad <- glm(formula = reg_outcome ~ reg_meropenem, family = "binomial", data = shock)
 shkRegUnad <- glm(formula = reg_outcome ~ reg_steroids, family = "binomial", data = shock)
+shkRegUnad <- glm(formula = reg_outcome ~ mx_dopamine, family = "binomial", data = shock); summary(shkRegUnad)
+shkRegUnad <- glm(formula = reg_outcome ~ mx_vasopressor, family = "binomial", data = shock); summary(shkRegUnad)
 shkRegUnad <- glm(formula = reg_outcome ~ reg_mod_anemia, family = "binomial", data = shock)
 shkRegUnad <- glm(formula = reg_outcome ~ reg_hai_hap, family = "binomial", data = shock)
 shkRegUnad <- glm(formula = reg_outcome ~ reg_sev_pneumonia, family = "binomial", data = shock)
 shkRegUnad <- glm(formula = reg_outcome ~ reg_sclerema, family = "binomial", data = shock)
-shkRegUnad <- glm(formula = reg_outcome ~ gap_shk_bt_3h, family = "binomial", data = shock)
-
-summary(shkRegUnad)
+shkRegUnad <- glm(formula = reg_outcome ~ gap_shk_bt_3h, family = "binomial", data = shock); summary(shkRegUnad)
 
 # OR & 95% CI
 round(exp(cbind(coef(shkRegUnad), confint(shkRegUnad))), 3)
@@ -503,7 +445,7 @@ round(exp(cbind(coef(shkRegUnad), confint(shkRegUnad))), 3)
 #                   family = "binomial" (link="logit"), 
 #                   data = shock)
 
-shkRegAdj <- glm(formula = reg_outcome ~ reg_meropenem + reg_steroids + gap_shk_bt_3h 
+shkRegAdj <- glm(formula = reg_outcome ~ reg_meropenem + mx_dopamine + reg_steroids + gap_shk_bt_3h
                  + reg_hai_hap + reg_sev_pneumonia + reg_sclerema
                  + inv_hem_hb + inv_hem_neut 
                  + inv_biochem_na + inv_biochem_k + inv_biochem_ca + inv_biochem_mg + inv_biochem_cr
@@ -511,12 +453,9 @@ shkRegAdj <- glm(formula = reg_outcome ~ reg_meropenem + reg_steroids + gap_shk_
                  family = "binomial" (link="logit"), 
                  data = shock); summary(shkRegAdj)
 
-# shkRegUnadj <- glm(formula = reg_outcome ~ pp_age_total_mnth, family = "binomial" (link="logit"), data = shock); summary(shkRegUnadj)
-# round(exp(cbind(coef(shkRegUnadj), confint(shkRegUnadj))), 3)
-
-shkRegAdj <- glm(formula = reg_outcome ~ reg_meropenem + reg_steroids + inv_biochem_ca + inv_biochem_k + inv_hem_neut, 
-                 family = "binomial" (link="logit"), 
-                 data = shock); summary(shkRegAdj)
+shkRegAdj <- glm(formula = reg_outcome ~ reg_meropenem + mx_dopamine + reg_steroids 
+                 + inv_biochem_ca + inv_biochem_k + inv_hem_neut,
+                 family = "binomial" (link="logit"), data = shock); summary(shkRegAdj)
 
 
 # OR & 95% CI
@@ -577,6 +516,8 @@ coefplot(shkRegAdj,
          newNames = c(reg_sclerema = "Sclerema",
                       reg_meropenem = "Fourth-line antibiotics",
                       reg_steroids = "Corticosteroids",
+                      mx_dopamine = "Dopamine",
+                      mx_vasopressor = "Vasopressors",
                       reg_mod_anemia = "Moderate anemia",
                       reg_sev_pneumonia = "Severe Pneumonia",
                       reg_hai_hap = "Hosp. acquired infection",
